b3d8c09990aef38b26d5350aecf978b7f6b2237d
Evgeniy Vasiliev <vasilievdevelop@gmail.com>
2024-04-18 21:26:19 +0300

    return const redirect_uri

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,7 @@ import {
   CLIENT_ID,
   RESPONSE_TYPE,
   RANDOM_STRING,
-  // REDIRECT_URI,
+  REDIRECT_URI,
   SCOPE,
 } from './const.js';
 
@@ -12,10 +12,7 @@ const searchParams = new URLSearchParams('');
 searchParams.append('client_id', CLIENT_ID);
 searchParams.append('response_type', RESPONSE_TYPE);
 searchParams.append('state', RANDOM_STRING);
-searchParams.append(
-  'redirect_uri',
-  'https://react-course-blogget.vercel.app/auth'
-);
+searchParams.append('redirect_uri', REDIRECT_URI);
 searchParams.append('scope', SCOPE);
 
 export const authURL = `${URL_AUTH}${searchParams.toString()}`;
